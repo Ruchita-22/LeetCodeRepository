b729227cc55d848f4eca521f36aa201c272c76a5
Ruchita-22 <ruchita.chandel22@gmail.com>
2023-06-22 01:50:35 +0530

    Time: 22 ms (48.44%), Space: 54.2 MB (95.31%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -13,7 +13,10 @@ class Pair{
 }
 class Solution {
     public long minCost(int[] nums, int[] cost) {
-        int n = nums.length;
+        // total elements in nums array
+        int n = nums.length;    
+        
+        // create a list and stor a pair <element, cost> and also count total elements if cost is freq
         ArrayList<Pair> list = new ArrayList<>();
         long total_elements = 0;
         for(int i = 0; i < n; i++){
@@ -21,17 +24,23 @@ class Solution {
             total_elements += cost[i];
         }
         
+        // sort the list acc. to elment in inc order
         Collections.sort(list, (o1,o2) -> o1.x != o2.x ? o1.x - o2.x : o1.y-o2.y);
         
+        
+        // mid elemet index
         total_elements = total_elements %2 == 0 ?  total_elements/2 : total_elements/2 + 1;
+        
+        // find mid element which will be center
         long mid = 0;
         int i=0;
-        
         while(i < n && mid < total_elements){
             mid +=  list.get(i).y;
             i++;
         }
         int center = list.get(i-1).x;
+        
+        // cal cost
         long total_cost = 0;
         for(Pair p : list){
             total_cost += ( Math.abs(p.x - center) * 1L * p.y);

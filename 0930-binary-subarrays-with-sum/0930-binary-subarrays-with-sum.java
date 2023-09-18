6aa32aa93f8e4d79cb8e6c854acb983a56155819
Ruchita-22 <ruchita.chandel22@gmail.com>
2023-09-19 04:00:30 +0530

    Time: 22 ms (31.51%), Space: 47.8 MB (40.72%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int numSubarraysWithSum(int[] arr, int goal) {
-        return countSubArrayWithSumK(arr, arr.length, goal);
+        return countSubArrayWithSumK(arr, goal);
     }
-    private static int countSubArrayWithSumK(int arr[], int n, int k)
+    private static int countSubArrayWithSumK(int arr[], int k)
     {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0,1);
         int count = 0;
         int sum = 0;
         int j=0;
-        while (j < n) {
+        while (j < arr.length) {
             sum += arr[j];
             int removeSum=sum-k;
             if (map.containsKey(removeSum))

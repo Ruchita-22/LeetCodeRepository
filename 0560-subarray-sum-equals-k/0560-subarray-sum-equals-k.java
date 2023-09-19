1de22baa6115e47413def1317d8c9b6cf9d7653a
Ruchita-22 <ruchita.chandel22@gmail.com>
2023-09-20 02:39:08 +0530

    Time: 22 ms (89.86%), Space: 46.4 MB (42.32%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();        // sum freq
+        map.put(0,1);
+        
+        int sum = 0, count = 0;
+        for(int e : arr){
+            sum += e;
+            int removeSum = sum - k;
+            if(map.containsKey(removeSum)){
+                count += map.get(removeSum);
+            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
+        }
+        return count;  
+    }
+}

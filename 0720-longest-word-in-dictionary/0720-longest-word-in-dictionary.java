353c5423930ae476cf789b8384bd8a66c4f52e81
Ruchita-22 <ruchita.chandel22@gmail.com>
2023-09-24 00:20:21 +0530

    Time: 5 ms (99.29%), Space: 43.7 MB (67.68%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -0,0 +1,43 @@
+class TrieNode{
+	    TrieNode children[] =  new TrieNode[26];
+	    //int pf = 0;
+	    int isEnd = 0;
+        String word = null;
+}
+class Solution {
+    public String longestWord(String[] dictionary) {
+        TrieNode root = new TrieNode();
+        for(int i = 0; i < dictionary.length; i++){
+            insert(root, dictionary[i]);
+        }
+        res = "";
+        res = query(root);
+        return res;
+    }
+    private static void insert(TrieNode root, String s){
+        TrieNode curr = root;
+        
+        for(int i = 0; i < s.length(); i++){
+            char ci = s.charAt(i);
+            if(curr.children[ci-'a'] == null){
+                curr.children[ci - 'a'] = new TrieNode();
+            }
+            curr = curr.children[ci-'a'];
+            //curr.pf++;
+        }
+        curr.isEnd = 1;
+        curr.word = s;
+    }
+    static String res;
+    private static String query(TrieNode root){
+        TrieNode curr = root;
+        for(int i = 0; i < 26; i++){
+            if(curr.children[i] != null && curr.children[i].word != null){
+                if(curr.children[i].word.length() > res.length())
+                    res = curr.children[i].word;
+                query(curr.children[i]);
+            }
+        }
+        return res;
+    }    
+}

ff44b45e044e1dc6075972e1af1dc25379a2c0b4
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-01-19 16:13:44 +0530

    Time: 17 ms (42.91%), Space: 46.3 MB (16.66%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,17 +1,37 @@
 class Solution {
-    public int[] nextGreaterElements(int[] arr) {
-        int arr1[] = new int[2*arr.length];
+//     public int[] nextGreaterElements(int[] arr) {
+//         int arr1[] = new int[2*arr.length];
         
-        for(int i = 0; i < arr.length; i++) {
-           arr1[i] = arr[i];
-           arr1[arr.length+i] = arr[i]; 
-        }  
-        int ngr[] = ngr(arr1);
-        //print(ngr);
+//         for(int i = 0; i < arr.length; i++) {
+//            arr1[i] = arr[i];
+//            arr1[arr.length+i] = arr[i]; 
+//         }  
+//         int ngr[] = ngr(arr1);
+//         //print(ngr);
+//         int res[] = new int[arr.length];
+//          for(int i = 0; i < arr.length; i++) {
+//            res[i] = ngr[i];
+//         }  
+//         return res;
+        
+
+//     }
+     public int[] nextGreaterElements(int[] arr) {
+         
+         Stack<Integer> stack = new Stack();
+        
+        for(int i = arr.length-1; i >= 0; i--) {
+            while(stack.size() > 0 && stack.peek() <= arr[i]) stack.pop();
+            stack.add(arr[i]);
+        }
+         
         int res[] = new int[arr.length];
-         for(int i = 0; i < arr.length; i++) {
-           res[i] = ngr[i];
-        }  
+         
+        for(int i = arr.length-1; i >= 0; i--) {
+            while(stack.size() > 0 && stack.peek() <= arr[i]) stack.pop();
+            res[i] = stack.size() > 0 ? stack.peek() : -1;
+            stack.add(arr[i]);
+        }
         return res;
         
 

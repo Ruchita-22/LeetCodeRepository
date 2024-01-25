7b605559a3d7516b32790919529df7b5887ab76b
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-01-25 20:28:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (48.19%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        
+        return solve(n, dp);
+    }
+    private static int solve(int n, int dp[]) {
+        if(n < 2)  return dp[0] = n;
+    
+        if(dp[n] != -1)  return dp[n];
+        
+        return dp[n] = solve(n - 1, dp) + solve(n -2, dp); 
+    }
+}

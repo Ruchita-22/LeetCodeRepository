9a2414e6f75f02b4520a82c90d169963486ab1d5
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-01-25 20:52:55 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (60.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
-    int dp[] = null;
     public int climbStairs(int n) {
-        dp = new int[n+1];
+        int dp[] = new int[n+1];
         Arrays.fill(dp,-1);
-        return solve(n);
+        
+        return solve(n, dp);
     }
-    public int solve(int n) {
+    private static int solve(int n, int dp[]) {
         if(n < 3)   return n;
         if(dp[n] != -1) return dp[n];
-        return dp[n] = solve(n-1) + solve(n-2);
+        
+        return dp[n] = solve(n-1, dp) + solve(n-2,dp);
     }
 }

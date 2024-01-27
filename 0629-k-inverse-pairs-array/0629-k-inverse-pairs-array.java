e9a44a7b183394c08b3fd763ce8e8b90123903c2
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-01-28 00:24:05 +0530

    Time: 959 ms (38.66%), Space: 52.2 MB (22.69%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -1,10 +1,11 @@
 class Solution {
     static int mod = 1000000000+7;
     public int kInversePairs(int n, int k) {
-       long dp[][] = new long[n+1][k+1]; 
-        for(long t[] : dp) Arrays.fill(t, -1);
+       // long dp[][] = new long[n+1][k+1]; 
+       //  for(long t[] : dp) Arrays.fill(t, -1);
         
-       return (int)(solve(n, k, dp) % mod); 
+       // return (int)(solve(n, k, dp) % mod); 
+       return (int)(solve(n, k) % mod); 
         
     }
     public long solve(int n, int k, long dp[][]) {
@@ -16,5 +17,25 @@ class Solution {
             res += solve(n-1, k - inv, dp) % mod;
         }
         return dp[n][k] = res;
+    }
+    // tabulation
+    public long solve(int n, int k) {
+        
+        long dp[][] = new long[n+1][k+1]; 
+        
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        
+       for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= k; j++) {
+                long res = 0;
+                for(int inv = 0; inv <= Math.min(j,i - 1); inv++) {
+                    res += dp[i-1][j - inv] % mod;
+                }
+                dp[i][j] = res;
+            }
+       } 
+       return dp[n][k]; 
     }
 }

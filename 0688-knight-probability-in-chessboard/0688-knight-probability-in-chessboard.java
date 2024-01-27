ca16209501e25f59c7fb805f4c23b00dc4d6fb1a
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-01-28 02:15:58 +0530

    Time: 6 ms (78.86%), Space: 42.3 MB (39.81%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,15 +1,13 @@
 class Solution {
-    // int dir[][] = new int[][]{{-2, -1}, {-2, +1}, {-1, -2}, {-1, +2}, {+1, -1}, {+1, +2}, {+2,-1}, {+2, +1}};
+    int dir[][] = new int[][]{{-2, -1}, {-2, +1}, {-1, -2}, {-1, +2}, {+1, -2}, {+1, +2}, {+2,-1}, {+2, +1}};
     
     public double knightProbability(int n, int k, int row, int column) {
         double dp[][][] = new double[n+1][n+1][k+1];
-        // for(double t[][] : dp) {
-        //     for(double t1[] : t)  Arrays.fill(t1, 0);
-        // }
-        //return solve(row, column, k, n, dp);
-        return solveMemo(n, k, row, column, dp);
+     
+        return solve(row, column, k, n, dp);
+      
     }
-    /*
+    
     public double solve(int i, int j, int k, int n, double dp[][][]) {
         
         if(!isValid(i, j, n))   return 0.0;
@@ -32,32 +30,6 @@ class Solution {
         if(i < 0 || i >= n || j < 0 || j >= n ) return false;
         else return true;
     }
-    */
     
-     int[][] dir = { { 2, 1 }, { 1, 2 }, { -1, 2 }, { -2, 1 }, { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 } };
-
-   
-    // DP + MEMOIZATION
-    private double solveMemo(int n, int k, int row, int column, double[][][] dp) {
-        // Base Case
-        if (row < 0 || column < 0 || row >= n || column >= n) {
-            return 0;
-        }
-        if (k == 0) {
-            return 1;
-        }
-
-        // step-2 => If dp[][][] is not null then just return its value
-        if (dp[row][column][k] != 0)
-            return dp[row][column][k];
-
-        // step-3 => if not calculated yet just calculate it and return it.
-        double temp = 0;
-        for (int i = 0; i < 8; i++) {
-            int x_row = row + dir[i][0];
-            int y_col = column + dir[i][1];
-            temp += solveMemo(n, k - 1, x_row, y_col, dp);
-        }
-        return dp[row][column][k] += (temp / 8.0);
-    }
+    
 }

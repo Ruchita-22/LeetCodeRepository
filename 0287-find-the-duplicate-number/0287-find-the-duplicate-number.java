972f93a6fbfd3b4316fae319d362d6dd73d117e1
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-03-25 01:39:32 +0530

    Time: 5 ms (68.67%), Space: 58 MB (68.80%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,54 +1,67 @@
 class Solution {
-    public int findDuplicate(int[] arr) {
-        int i=0;
-        while(i<arr.length){
-            int x = arr[i];
-            if(x == i+1)   i++; 
-            else{
-                if(x == arr[x-1] || x == arr[x])   return x;
-                else{ 
-                    int a = x;
-                    int b = arr[x];
-                    arr[x] = a;
-                    arr[i] = b;    
-                }
-            }
+  
+//     public int findDuplicate(int[] arr) {
+//         int i=0;
+//         while(i<arr.length){
+//             int x = arr[i];
+//             if(x == i+1)   i++; 
+//             else{
+//                 if(x == arr[x-1] || x == arr[x])   return x;
+//                 else{ 
+//                     int a = x;
+//                     int b = arr[x];
+//                     arr[x] = a;
+//                     arr[i] = b;    
+//                 }
+//             }
           
-        }
-        return -1;
+//         }
+//         return -1;
         
+//     }
+    public int findDuplicate(int[] nums) {
+        
+        int fast = nums[0];
+        int slow = nums[0];
+        slow =  nums[slow];
+        fast = nums[nums[fast]];
+        
+        while(fast!=slow ){
+            slow =  nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        fast = nums[0];
+           
+        while(fast != slow ){
+            slow =  nums[slow];
+             fast = nums[fast];
+        }
+        return slow;
     }
 }
 
-
 /*
-- no repeattaion of elements
--
-n+1 = 5 
-    1-4
-    
-arr = 1 2 3 4
-    
- num =   1 3 4 2 2  
- n+1 =5
- 1-5
-    
-    =2
-    
-    x^x = 0
-    0^x = x
-    
-    Tc = O(n)
-    
-    sorting = nlog + n = nlog n 
-        sc = O(1); 
-        
-    hashset  = n 
-    sc = n
-    
-    xor = 
-    
-    
-    
-    
- */   
+approach 1
+sort array and check adjacent element
+tc = O(nlogn)
+sc = O(1)
+array modified
+
+approach 2
+using hashset
+tc = O(n);
+sc = O(n);
+
+approach 3 
+index matching
+tc=O(n)
+sc=O(1)
+array modified
+
+slow and fast pointer
+tc = O(n)
+sc = O(1)
+
+
+*/

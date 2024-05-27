0f0fa602c6648fb92daed9bc1ffaedb57be243fd
Ruchita-22 <ruchita.chandel22@gmail.com>
2024-05-27 19:12:28 +0530

    Time: 3 ms (13.96%), Space: 41.3 MB (29.54%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,27 @@
+class Solution {
+    	public int specialArray(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        for (int x = 1; x <= nums.length; x++) {
+            int count = 0;
+            for (int num : nums) {
+                if (num >= x) {
+                    count++;
+                }
+            }
+            
+            if (count == x) {
+                return x;
+            }
+        }
+        return -1;
+    }
+}
+
+
+    
+    
+    
+    
+    

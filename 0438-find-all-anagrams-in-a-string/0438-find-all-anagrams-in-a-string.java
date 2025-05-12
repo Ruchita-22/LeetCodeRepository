ae5b9accb110614c8d3ae7eb6ccbd785db8d2d8e
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-05-13 02:15:10 +0530

    Time: 9 ms (90.47%), Space: 44.5 MB (96.76%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,39 +1,30 @@
 class Solution {
-    public List<Integer> findAnagrams(String s1, String s2) {
-        if(s1.length() < s2.length())   return new ArrayList();
+    public List<Integer> findAnagrams(String s, String p) {
+        if(s.length() < p.length()) return new ArrayList<>();
         
-        int fs1[] = new int[26];
-        int fs2[] = new int[26];
-        
-        for(char c : s2.toCharArray())  fs2[c-'a']++;
-        
-        int k = s2.length();
-        
-        // first window
-        for(int j = 0; j < k; j++){
-            char cj = s1.charAt(j);
-            fs1[cj - 'a']++;
-        }
         List<Integer> list = new ArrayList<>();
-        
-        if(Arrays.equals(fs1,fs2))  list.add(0);
-        
-        int i = 0, j = k;
-        
-        // slide and check
-        while(j < s1.length()){
-            char ci = s1.charAt(i);
-            char cj = s1.charAt(j);
-            fs1[ci - 'a']--;
-            fs1[cj - 'a']++;
+        int fs1[] = new int[26]; 
+        int fs2[] = new int[26]; 
+        for(char c : p.toCharArray()) {
+            fs1[c-'a']++;
+        }
+
+        int i = 0, j = 0;
+        for( j = 0; j < p.length(); j++) {
+            char c = s.charAt(j);
+            fs2[c-'a']++;
+        }
+        if(Arrays.equals(fs1,fs2)) list.add(i);
+
+        while(j < s.length()) {
+            char ci = s.charAt(i);
+            char cj = s.charAt(j);
+            fs2[ci-'a']--;
+            fs2[cj-'a']++;
             i++; j++;
-            if(Arrays.equals(fs1,fs2))  list.add(i);
-            
+            if(Arrays.equals(fs1,fs2)) list.add(i);
         }
         return list;
-        
-        
-        
         
         
     }

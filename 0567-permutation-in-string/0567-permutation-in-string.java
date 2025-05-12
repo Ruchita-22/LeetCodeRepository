ec2e30d9d5879351113318dbf502d8c927857e3a
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-05-13 02:23:14 +0530

    Time: 7 ms (74.52%), Space: 42.9 MB (52.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,35 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s2.length() < s1.length())   return false;
-        
+
         int fs1[] = new int[26];
         int fs2[] = new int[26];
         
-        for(char c : s1.toCharArray())  fs1[c-'a']++;
-        
-        int k = s1.length();
-        
-        // first window
-        for(int j = 0; j < k; j++){
+        for(char c : s1.toCharArray()) fs1[c-'a']++;
+
+        int i = 0, j = 0;
+        for(j = 0; j < s1.length(); j++) { 
             char cj = s2.charAt(j);
-            fs2[cj - 'a']++;
+            fs2[cj-'a']++;
         }
-       
-        
         if(Arrays.equals(fs1,fs2))  return true;
-        
-        int i = 0, j = k;
-        
-        // slide and check
-        while(j < s2.length()){
+
+        while(j < s2.length()) {
             char ci = s2.charAt(i);
+            fs2[ci-'a']--;
+
             char cj = s2.charAt(j);
-            fs2[ci - 'a']--;
-            fs2[cj - 'a']++;
+            fs2[cj-'a']++;
+
+            if(Arrays.equals(fs1,fs2))  return true;
+
             i++; j++;
-            if(Arrays.equals(fs1,fs2)) return true;
-            
         }
-        return false;   
+        return false;
+        
     }
 }

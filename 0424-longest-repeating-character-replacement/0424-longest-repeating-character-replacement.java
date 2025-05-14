5d0a588d68a3a4c619e090558b25ed806dce8b3d
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-05-15 00:25:14 +0530

    Time: 11 ms (64.5%), Space: 44.8 MB (72.65%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public int characterReplacement(String s, int k) {
         
@@ -6,10 +7,12 @@ class Solution {
         int largestCount = 0, i = 0, maxlen = 0;
         
         for(int j = 0; j < s.length(); j ++){
-            arr[s.charAt(j) - 'A']++;
-            largestCount = Math.max(largestCount, arr[s.charAt(j) - 'A']);
+			char ci = s.charAt(i);
+			char cj = s.charAt(j);
+            arr[cj - 'A']++;
+            largestCount = Math.max(largestCount, arr[cj - 'A']);
             if(j - i + 1 - largestCount > k){     
-                arr[s.charAt(i) - 'A']--;
+                arr[ci - 'A']--;
                 i ++;
             }
             maxlen = Math.max(maxlen, j - i + 1);     

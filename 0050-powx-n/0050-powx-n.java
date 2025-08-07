9721bf22f2576b0694637f379761f3d793292538
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-08-08 03:46:09 +0530

    Time: 0 ms (100%), Space: 43.4 MB (10.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,15 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1.0;
-        if(x == 1)  return 1.0;
-        if(n < 0)   return 1.0/myPow(x,-n);
+        // Convert n to long to safely handle Integer.MIN_VALUE
+        return myPow(x, (long)n);
+    }
 
-        double pow = myPow(x, n/2);
+    private double myPow(double x, long n) {
+        if (n == 0) return 1.0;
+        if (x == 1.0) return 1.0;
+
+        if (n < 0) return 1.0 / myPow(x, -n);
+
+        double pow = myPow(x, n / 2);
         pow = pow * pow;
+
         if (n % 2 == 0)
             return pow;
         else 
-            return pow * x;    
-            
+            return pow * x;
     }
 }

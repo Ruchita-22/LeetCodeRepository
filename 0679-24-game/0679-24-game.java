463eb45bd0b58b10da72996d6bb9bb5900269460
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-08-19 01:48:50 +0530

    Time: 25 ms (5.69%), Space: 44.6 MB (61.87%) - LeetHub

--- 0679-24-game/0679-24-game.java
+++ 0679-24-game/0679-24-game.java
@@ -1,40 +1,39 @@
 class Solution {
-
-    boolean res = false;
     final double eps = 0.001;
 
-    public boolean judgePoint24(int[] nums) {
-        List<Double> arr = new ArrayList<>();
-        for(int n: nums) arr.add((double) n);
-        helper(arr);
-        return res;
+    public boolean judgePoint24(int[] cards) {
+         List<Double> arr = new ArrayList<>();
+        for(int n: cards) arr.add((double) n);
+        return solve(arr, arr.size());
+        
     }
-
-    private void helper(List<Double> arr){
-        if(res) return;
-        if(arr.size() == 1){
-            if(Math.abs(arr.get(0) - 24.0) < eps)
-                res = true;
-            return;
+    private boolean solve(List<Double> arr, int n) {
+        if(arr.size() == 1)   {
+            return Math.abs(arr.get(0) - 24.0) < eps;
         }
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                List<Double> next = new ArrayList<>();
-                Double p1 = arr.get(i), p2 = arr.get(j);
-                next.addAll(Arrays.asList(p1+p2, p1-p2, p2-p1, p1*p2));
-                if(Math.abs(p2) > eps)  next.add(p1/p2);
-                if(Math.abs(p1) > eps)  next.add(p2/p1);
-                
-                arr.remove(i);
-                arr.remove(j);
-                for (Double n: next){
-                    arr.add(n);
-                    helper(arr);
-                    arr.remove(arr.size()-1);
+        for(int i = 0; i <  n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j)  continue;
+                double a = arr.get(i), b = arr.get(j);
+                List<Double> remaining = new ArrayList<>();
+                for(int k = 0; k < n; k++) {
+                    if(k != i && k != j) remaining.add(arr.get(k));
                 }
-                arr.add(j, p2);
-                arr.add(i, p1);
+                List<Double> list = new ArrayList<>(Arrays.asList(
+                    a+b,a-b, b-a, a*b
+                ));
+                if(Math.abs(b) > eps)  list.add(a/b);
+                if(Math.abs(a) > eps)  list.add(b/a);
+            
+                for(Double d : list) {
+                    remaining.add(d);
+                    if(solve(remaining,remaining.size())==true)
+                        return true;
+                    remaining.remove(remaining.size()-1);
+                }
+
             }
         }
+        return false;
     }
 }

47f62b8cdfb413b53d4f47c33cba3c5f97d045d1
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-08-21 07:33:15 +0530

    Time: 0 ms (100%), Space: 40.7 MB (35.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,7 +6,7 @@ class Solution {
         
     }
     private int solve(int i, int j, int dp[][]) {
-        if(i == 0 || j == 0)    return 1;
+        if(i == 0 && j == 0)    return 1;
         if(i < 0 || j < 0)  return 0;
         if(dp[i][j] != -1)  return dp[i][j];
         return dp[i][j] = solve(i-1,j, dp)+solve(i, j-1, dp);

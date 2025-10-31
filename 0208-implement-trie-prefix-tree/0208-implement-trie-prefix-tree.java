7b41abbe532b1be22c0ab1965c00fb8782bca361
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-01 03:34:33 +0530

    Time: 30 ms (99.72%), Space: 61.9 MB (5.37%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,50 +1,43 @@
 class TrieNode {
-    TrieNode children[] = new TrieNode[26];
-    int pf = 0;
-    int  isEnd = 0;
+    TrieNode child[] = new TrieNode[26];
+    int pf = 0, isEnd = 0;
 }
-
 class Trie {
     TrieNode root;
+
     public Trie() {
         root = new TrieNode();
     }
     
-    public void insert(String s) {
+    public void insert(String word) {
         TrieNode curr = root;
-        for( int i = 0; i < s.length(); i++){
-            char ci = s.charAt(i);
-            if(curr.children[ci-'a'] == null){
-                curr.children[ci-'a']   =   new TrieNode();
+        for(char c : word.toCharArray()) {
+            if(curr.child[c-'a'] == null) {
+                curr.child[c-'a'] = new TrieNode();
+                curr.pf++;
             }
-            curr = curr.children[ci-'a'];
-            curr.pf++;
+            curr = curr.child[c-'a'];
         }
         curr.isEnd = 1;
+        
     }
     
-    public boolean search(String s) {
+    public boolean search(String word) {
         TrieNode curr = root;
-        for( int i = 0; i < s.length(); i++){
-            char ci = s.charAt(i);
-            if(curr.children[ci-'a'] == null){
-                return false;
-            }
-            curr = curr.children[ci-'a'];
+        for(char c : word.toCharArray()) {
+            if(curr.child[c-'a'] == null) return false;
+            curr = curr.child[c-'a'];
         }
-        return curr.isEnd == 1 ? true : false;
+        return curr.isEnd == 1;
     }
     
-    public boolean startsWith(String s) {
+    public boolean startsWith(String prefix) {
         TrieNode curr = root;
-        for( int i = 0; i < s.length(); i++){
-            char ci = s.charAt(i);
-            if(curr.children[ci-'a'] == null){
-                return false;
-            }
-            curr = curr.children[ci-'a'];
+        for(char c : prefix.toCharArray()) {
+            if(curr.child[c-'a'] == null) return false;
+            curr = curr.child[c-'a'];
         }
-        return curr.pf > 0  ? true : false;
+        return true;
     }
 }
 

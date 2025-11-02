5549ee6970d55eca75d08d01841f00e02bafc21c
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-03 01:49:10 +0530

    Time: 820 ms (52.08%), Space: 234.1 MB (5.15%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,57 +1,58 @@
-class TrieNode{
-    TrieNode children[] = new TrieNode[2];
+class TrieNode {
+    TrieNode child[] = new TrieNode[2];
     int isEnd = 0;
 }
 class Solution {
-    public int findMaximumXOR(int[] arr) {
-        TrieNode root = new TrieNode();
-        for(int e : arr){
-            insert(root, e);
+    TrieNode root = new TrieNode();
+    public int findMaximumXOR(int[] nums) {
+        for(int e : nums) {
+            insert(e);
         }
         int ans = 0;
-        for(int e : arr){
-            ans = Math.max(ans, search(root, e));
+        for(int e : nums) {
+            ans = Math.max(ans,search(e));
         }
-        return ans;
-    }
-    private static int search(TrieNode root, int n){
-        TrieNode curr = root;
-        int ans = 0;
-        for(int i = 31; i >= 0; i--){
-            if(curr == null)    break;
-            if(checkBit(n, i) == 1){
-                if(curr.children[0] !=  null){
-                   curr = curr.children[0];
-                    ans |= (1<<i); 
-                }
-                else curr = curr.children[1];
-            }
-            else {
-                if(curr.children[1] !=  null){
-                   curr = curr.children[1];
-                    ans |= (1<<i); 
-                }
-                else curr = curr.children[0];
-                
-            }
-        }
-        return ans;
-    }
-    private static void insert(TrieNode root, int n){
-        TrieNode curr = root;
         
-        for(int i = 31; i >= 0; i--){
-            int currBit = checkBit(n , i);
-            if(curr.children[currBit] == null){
-                curr.children[currBit] =    new TrieNode(); 
+        return ans;
+    }
+    private void insert(int num) {
+        TrieNode curr = root;
+        for(int i = 31; i >= 0; i--) {
+            if(curr == null) break;
+            int p = checkBit(num, i);
+            if(curr.child[p] == null) {
+                curr.child[p] = new TrieNode();
             }
-            curr = curr.children[currBit];
+            curr = curr.child[p];
         }
         curr.isEnd = 1;
     }
-    
-    private static int checkBit(int x, int i){
-		if (((x >> i) & 1) == 1)  return 1;
-        else return 0;
+    private int search(int num) {
+        TrieNode curr = root;
+        int ans = 0;
+        for(int i = 31; i >= 0; i--) {
+            if(curr == null)    break;
+            int p = checkBit(num, i);
+            if(p == 1) {
+                if(curr.child[0] != null) {
+                    curr = curr.child[0];
+                    ans |= (1<<i);
+                } else if(curr.child[1]!=null) {
+                    curr = curr.child[1];
+                }
+            }
+            if(p == 0) {
+                if(curr.child[1] != null) {
+                    curr = curr.child[1];
+                    ans |= (1<<i);
+                } else if(curr.child[0]!=null) {
+                    curr = curr.child[0];
+                }
+            }
+        }
+        return ans;
+    }
+    private int checkBit(int num, int i) {
+        return ((num>>i) & 1) == 1 ? 1 : 0;
     }
 }

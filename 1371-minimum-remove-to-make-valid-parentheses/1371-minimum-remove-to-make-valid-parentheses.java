4d0839628349f64f0d37b030443ac36ce521c93d
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-07 00:58:19 +0530

    Time: 473 ms (8.63%), Space: 47.2 MB (6.86%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -2,7 +2,7 @@ class Solution {
 
     public String minRemoveToMakeValid(String s) {
 
-        return minRemoveToMakeValid1(s);
+        return minRemoveToMakeValid2(s);
     }
     //approach 1
     private String minRemoveToMakeValid1(String s) {
@@ -39,5 +39,41 @@ class Solution {
         }
 
         return new String (sb);
+    }
+    //approach 2
+    private String minRemoveToMakeValid2(String s) {
+        var open = 0;
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                open++;
+                sb.append(c);
+            } else if(c == ')') {
+                if(open > 0) {
+                    open--;
+                    sb.append(c);
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        var close = 0;
+        s = new String(sb);
+        sb = new StringBuilder();
+        for(int i = s.length()-1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if(c == ')') {
+                close++;
+                sb.insert(0,c);
+            } else if(c == '(') {
+                if(close > 0) {
+                    close--;
+                    sb.insert(0,c);
+                }
+            } else {
+                sb.insert(0,c);
+            }
+        }
+        return new String(sb);
     }
 }

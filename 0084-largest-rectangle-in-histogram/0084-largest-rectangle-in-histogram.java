0796066f9789bed94ee14da9345fdd555b464d44
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-09 14:53:26 +0530

    Time: 112 ms (20.61%), Space: 76.3 MB (8.42%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,36 +1,35 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int nslIndex[] = nslIndex(heights);
-        int nsrIndex[] = nsrIndex(heights);
-        int ans = Integer.MIN_VALUE;
+        int[] nsr_index = nsr_index(heights);
+        int[] nsl_index = nsl_index(heights);
+        int res = 0;
         for(int i = 0; i < heights.length; i++) {
-            int area = (nsrIndex[i] - nslIndex[i] - 1) * heights[i];
-            ans = Math.max(ans, area);
-            ans = Math.max(ans, heights[i]);
-        }
-        return ans;
-    }
-    public int[] nslIndex(int arr[]) {
-        int res[] = new int[arr.length];
-        Stack<Integer> stack = new Stack(); // store index
-        
-        for(int i = 0; i < arr.length; i++) {
-            while(stack.size() >0 && arr[stack.peek()] >= arr[i])
-                stack.pop();
-            res[i] = stack.size() > 0 ? stack.peek() : -1;
-            stack.push(i);
+            int area = heights[i] * (nsr_index[i] - nsl_index[i] - 1);
+            res = Math.max(res, area);
         }
         return res;
-    }
-    public int[] nsrIndex(int arr[]) {
-        int res[] = new int[arr.length];
-        Stack<Integer> stack = new Stack(); // store index
         
-        for(int i = arr.length-1; i >= 0; i--) {
-            while(stack.size() >0 && arr[stack.peek()] >= arr[i])
-                stack.pop();
-            res[i] = stack.size() > 0 ? stack.peek() : arr.length;
+    }
+    private int[] nsr_index(int[] arr) {
+        var stack = new Stack<Integer>();
+        int res[] = new int[arr.length];
+        for(int i = arr.length-1; i>= 0; i--) {
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? arr.length : stack.peek();
             stack.push(i);
+
+        }
+        return res;
+
+    }
+    private int[] nsl_index(int[] arr) {
+        var stack = new Stack<Integer>();
+        int res[] = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+
         }
         return res;
     }

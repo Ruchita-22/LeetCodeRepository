097d0745cc5787962f0379ccb4396d29b54774b7
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-12 03:07:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (13.62%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n < 0) return false;
-        int count = 0;
-        for(int i = 0; i < 32; i++) {
-            if((n & (1<<i)) != 0) {
-                count++;
-            }    
-        }
-        return count == 1;
+        if(n <= 0) return false;
+        // int count = n&(n-1);
+
+        // for(int i = 0; i < 32; i++) {
+        //     if((n & (1<<i)) != 0) {
+        //         count++;
+        //     }    
+        // }
+        return (n&(n-1)) == 0;
         
     }
 }

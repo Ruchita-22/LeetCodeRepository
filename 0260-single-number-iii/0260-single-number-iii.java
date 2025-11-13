d8bd58b90c4dedabfb2659c3bee87ff13292514d
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-14 00:12:06 +0530

    Time: 1 ms (99.91%), Space: 47.9 MB (9.74%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int[] singleNumber(int[] arr) {
-        int res = 0;
-        
-        for(int e : arr)    res ^= e;
-        
-        int pos = 0; 
+    public int[] singleNumber(int[] nums) {
+        if(nums.length < 3) return nums;
+        int xor = 0;
+        for(int n : nums) {
+            xor ^= n;
+        }
+        int pos = 0;
         for(int i = 0; i < 32; i++) {
-            if(((res>>i)&1)==1) {
-                pos = i;
+            if((xor & (1 << i)) != 0) {
+                pos = i; 
                 break;
             }
         }
         int set = 0, unset = 0;
-        
-        for(int  e : arr) {
-            if(((e >> pos) & 1) == 1) set ^= e;
-            else unset ^= e;
+        for(int n : nums) {
+            if((n & (1<<pos)) != 0) unset ^= n;
+            else set ^= n;
         }
-        return new int[]{set, unset};
+        return new int[]{set,unset};
         
     }
 }

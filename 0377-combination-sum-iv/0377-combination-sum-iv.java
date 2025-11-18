9e41fe7db6ce925edcb13fe246179710b5c4043e
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-18 11:11:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (8.02%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,20 +1,21 @@
 class Solution {
-
     public int combinationSum4(int[] nums, int target) {
-        int dp[] = new int[target+1];
+        int dp[]= new int[target+1];
         Arrays.fill(dp,-1);
-        return solve(nums, target, dp);
-
-        
+         
+        return solve(target, nums, dp);
     }
-    private int solve(int arr[], int target, int dp[]) {
-        if(target == 0) return dp[target] = 1;
-        if(target < 0)  return 0;
-        if(dp[target] != -1)    return dp[target];
+   
+    private int solve(int sum, int nums[], int dp[]) {
+        if(sum == 0)    return dp[sum] = 1;
+        if(sum < 0)  return 0;
+
+        if(dp[sum] != -1)    return dp[sum];
+
         int ans = 0;
-        for(int i = 0; i < arr.length; i++) {
-                ans += solve(arr, target-arr[i], dp);
+        for(int i = 0 ; i < nums.length; i++) {
+            ans += solve(sum-nums[i], nums, dp);
         }
-        return dp[target] = ans;
+        return dp[sum] = ans;
     }
 }

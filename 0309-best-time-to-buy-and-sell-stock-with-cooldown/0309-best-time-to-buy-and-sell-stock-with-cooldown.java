6ce49582ee0a706eaed638d71d2662daeb0a1f9f
Ruchita-22 <ruchita.chandel22@gmail.com>
2025-11-18 11:56:43 +0530

    Time: 1 ms (77.15%), Space: 43.7 MB (10.3%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int dp[][] = new int[prices.length+1][2+1];
+        for(int t[] : dp) {
+            Arrays.fill(t,-1);
+        } 
+        return solve(0, 1, prices, dp);
+    }
+    private int solve(int n, int buy, int nums[], int dp[][]) {
+        if(n >= nums.length)    return 0;
+        if(dp[n][buy] != -1)    return dp[n][buy];
+        if(buy == 1) { // buy or skip
+            return dp[n][buy] = Math.max(-nums[n] + solve(n+1, 0, nums, dp), solve(n+1, 1, nums, dp)); 
+
+        } else {    //sell or skip
+            return dp[n][buy] = Math.max(nums[n] + solve(n+2, 1, nums, dp), solve(n+1, 0, nums, dp)); 
+        }
+    }
+}

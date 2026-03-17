b5fa387484fc1841eb565692e967d5adfee8e139
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-03-17 18:58:14 +0530

    Time: 10 ms (12.26%), Space: 61.5 MB (91.37%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,35 +1,36 @@
 class Solution {
-    public int countSquares(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        
+    public int countSquares(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
         int dp[][] = new int[m+1][n+1];
-        for(int t[] : dp)   Arrays.fill(t, -1);
-        
-        int count = 0;
-        for(int i = 0; i < m ; i++) {
+        for(int t[] : dp) Arrays.fill(t, -1);
+
+        int ans = 0;
+        for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 1) {
-                    count += solve(i, j, mat, dp);
-                }
+                if(grid[i][j] == 1)
+                    ans += solve(i, j, grid, dp);
             }
         }
-        return count;
-    }
-    private static int solve(int i, int j, int mat[][], int dp[][]) {
-        if(! isValid(i,j, mat) || mat[i][j] == 0)   return 0;
         
+        return ans;
+        
+    }
+    private int solve(int i, int j, int[][] grid, int dp[][]) {
+    
+        if(!isValid(i,j, grid) || grid[i][j] == 0)  return 0;
+
         if(dp[i][j] != -1)  return dp[i][j];
-        
-        int x = 1 + solve(i+1, j, mat, dp);
-        int y = 1 + solve(i, j+1, mat, dp);
-        int z = 1 + solve(i+1, j+1, mat, dp);
-        return dp[i][j] = Math.min(x, Math.min(y,z));
+
+        int path1 = solve(i, j+1, grid, dp);
+        int path2 = solve(i+1, j+1, grid, dp);
+        int path3 = solve(i+1, j, grid, dp);
+        return dp[i][j] = 1 + Math.min(path1, Math.min(path2, path3));
     }
-    private static boolean isValid(int i, int j, int arr[][]) {
-        int m = arr.length;
-        int n = arr[0].length;
-        if(i < 0 || i >= m || j < 0 || j >= n)  return false;
-        else return true;
+    private boolean isValid(int i, int j, int grid[][]) {
+        if(i < 0 || i >= grid.length  || j < 0 || j >= grid[0].length) return false;
+        return true;
+
     }
 }

12b0e28a18fdd6ce4cdd9a408ed0c8f28ee4362f
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-04-18 14:03:29 +0530

    Time: 8 ms (91.28%), Space: 49.1 MB (43.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (o1,o2)-> o1[0] != o2[0] ? o1[0]-o2[0] : o1[1]-o2[1]);
+        if(intervals.length == 0) return new int[0][0];
 
-        ArrayList<int[]> list  = new ArrayList<>();
-        list.add(intervals[0]);
-        for(int i=1;i<intervals.length;i++){
-            int oldInterval[] = list.get(list.size()-1);
-            if(oldInterval[1]<intervals[i][0])  list.add(intervals[i]);
-            else{
-                oldInterval[0] = Math.min(oldInterval[0] , intervals[i][0]);
-                oldInterval[1] = Math.max(oldInterval[1] , intervals[i][1]);
-            }
-        }
-        return list.toArray(new int[list.size()][]);
+        // sort
+        Arrays.sort(intervals, (o1,o2)-> o1[0] != o2[0] ? o1[0] - o2[0] : o1[1] - o2[1]);
         
+        ArrayList<int[]> res = new ArrayList();
+        res.add(intervals[0]);
+        
+        for(int i = 1; i < intervals.length; i++) {
+
+            int[] oldInterval = res.get(res.size()-1);
+            if(oldInterval[1] < intervals[i][0]) {
+                res.add(intervals[i]);
+            } else if(oldInterval[1] >= intervals[i][0]) {
+                oldInterval[0] = Math.min(oldInterval[0], intervals[i][0]);
+                oldInterval[1] = Math.max(oldInterval[1], intervals[i][1]);
+            }
+            
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }

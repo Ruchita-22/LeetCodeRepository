7c6b214b76b887aa3ffb37270a81ba4fd0c73337
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-05-01 02:03:15 +0530

    Time: 43 ms (56.2%), Space: 63.6 MB (60.08%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,36 +1,37 @@
 class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String s2= reverse(s);
+        return longestCommonSubsequence(s, s2);
+    }
     private String reverse(String s) {
         char ch[] = s.toCharArray();
-        int i = 0, j = ch.length-1;
-        while(i <= j) {
-            char temp = ch[i];
+        int i = 0,  j = ch.length-1;
+        while(i < j) {
+            char c = ch[i];
             ch[i] = ch[j];
-            ch[j] = temp;
+            ch[j] = c;
             i++; j--;
         }
         return new String(ch);
     }
-    public int longestPalindromeSubseq(String s) {
-        String revStr = reverse(s);
+    public int longestCommonSubsequence(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        
+        int dp[][] = new int[m+1][n+1];
 
-        return longestCommonSubsequence(s, revStr);
-    }
-    private int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length()+1][text2.length()+1];
-        for(int t[] : dp)   Arrays.fill(t,-1);
+        for(int t[] : dp)   Arrays.fill(t, -1);
 
-        return solve(text1, text2, text1.length(), text2.length(), dp);
+        return solve(m-1, n-1, s1, s2, dp);
     }
-    private int solve(String str1, String str2, int m, int n, int dp[][]) {
-        if(m == 0 || n == 0)    return 0;
-        if( dp[m][n] != -1) return dp[m][n];
-        if(str1.charAt(m-1) == str2.charAt(n-1)) {
-            return dp[m][n] = 1 + solve(str1, str2, m-1, n-1, dp);
+    private int solve(int i, int j, String s1, String s2, int dp[][]) {
+        if(i < 0 || j < 0)  return 0;
+
+        if(dp[i][j] != -1)  return dp[i][j];
+        
+        if(s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = 1 + solve(i-1, j-1, s1, s2, dp);
         } else {
-            return  dp[m][n] = Math.max(
-                solve(str1, str2, m-1, n, dp),
-                solve(str1, str2, m, n-1, dp)
-            );
-        }
+            return dp[i][j] = Math.max(solve(i-1, j, s1, s2, dp), solve(i, j-1, s1, s2, dp));
+        }    
     }
 }

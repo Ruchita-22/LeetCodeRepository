5493da60ac534e968e268e6cbc9cc3a08bdfb2bf
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-05-24 11:08:30 +0530

    Time: 28 ms (25.86%), Space: 49.1 MB (12.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -3,13 +3,16 @@ class Solution {
         var map = new HashMap<Integer, Integer>();
         map.put(0,1);
         int sum = 0, count = 0;
-        for(int num : nums) {
-            sum += num;
-            if(map.containsKey(sum - k)) {
+
+        for(int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            
+            if(map.containsKey(sum-k)) {
                 count += map.get(sum-k);
             }
             map.put(sum, map.getOrDefault(sum,0)+1);
         }
         return count;
+        
     }
 }

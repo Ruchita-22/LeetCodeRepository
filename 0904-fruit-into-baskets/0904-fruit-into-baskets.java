b1ec88d67a9151ecf691ed78dffd218635d22346
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-05-24 09:05:37 +0530

    Time: 54 ms (66.13%), Space: 70.4 MB (64.06%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,26 +1,24 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        
-        var map = new HashMap<Integer,Integer>();
-        
-        int i=0,len=0;
-        
-        for(int j =0; j<fruits.length; j++){
-            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
-
-            while(map.size()>2){
-                int a = fruits[i];
-                map.put(a,map.get(a)-1);
-                if(map.get(a)==0)   map.remove(a);
-                i++;
-            }
-            len = Math.max(len,j-i+1);             
-                
-        }
-        // if(map.size()<2)
-        //     return map.get(fruits[i]);
-        return len;
+        return longestSubarrayWithKUniqueCharacter(fruits, 2);
     }
+    private int longestSubarrayWithKUniqueCharacter(int[] nums, int k) {
+		int i = 0, ans = 0;
+		var map = new HashMap<Integer, Integer>();
+		for (int j = 0; j < nums.length; j++) {
+			
+			map.put(nums[j], map.getOrDefault(nums[j],0)+1);
+
+			while(map.size() > k) {
+				map.put(nums[i], map.get(nums[i])-1);
+				if(map.get(nums[i]) == 0)	map.remove(nums[i]);
+				i++;
+			}
+			ans = Math.max(ans,j-i+1);
+		}
+		return ans;
+	}
+
 }
 //variation of len of the longest substring with k unique character
 //pick toy varaiation

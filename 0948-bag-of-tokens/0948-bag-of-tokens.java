a8ea684cb539ec83aaf6a8bcaf480bcce2b070d7
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-05-29 00:59:20 +0530

    Time: 6 ms (77.08%), Space: 45.4 MB (8.96%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,26 +1,20 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        if(tokens == null || tokens.length == 0 || power == 0) return 0;
         Arrays.sort(tokens);
-        // for(int e : tokens) System.out.println(e + " ");
-        // System.out.println();
-        if(tokens[0] > power) return 0;
-        
-        int score = 0, maxAns = 0;
         int i = 0, j = tokens.length-1;
+        int score = 0, maxScore = 0;
         while(i <= j) {
-            if(tokens[i] <= power) {
+            if(power >= tokens[i]) {
+                power -= tokens[i++];
                 score++;
-                maxAns = Math.max(maxAns, score);
-                power -= tokens[i];
-                i++;
-            } else {
+                maxScore = Math.max(maxScore,score);
+            } else if(score >= 1) {
+                power += tokens[j--];
                 score--;
-                power += tokens[j];
-                j--;   
+            } else {
+                return maxScore;
             }
         }
-        return maxAns;
-        
+        return maxScore;
     }
 }

eab8e37ab0db46951a3d369e0dbca5e45352a8fa
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-05-30 00:22:30 +0530

    Time: 2 ms (88.16%), Space: 43.8 MB (58.78%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int freq[] = new int[26];
+        int taken[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ci = s.charAt(i);
+            freq[ci-'a'] = i;
+        }
+
+
+        var stack = new Stack<Character>();
+        for (int i = 0; i < s.length() ; i++) {
+            char ci = s.charAt(i);
+            if(taken[ci-'a'] == 1) continue;
+
+            while(stack.size() > 0 && stack.peek() > ci && freq[stack.peek()-'a'] > i){
+                taken[stack.peek()-'a'] = 0;
+                stack.pop();
+            }
+            stack.push(ci);
+            taken[ci-'a'] = 1;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(stack.size() > 0) {
+            sb.insert(0,stack.pop());
+        }
+        return  sb.toString();
+    }
+}

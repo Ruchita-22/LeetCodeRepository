8ea7cdf8a72cae4413ca67b3125fcbcf50dec5c4
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-06-02 18:31:21 +0530

    Time: 15 ms (82.35%), Space: 46.3 MB (97.15%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,16 +1,35 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        for(int l = s.length()/2; l>0; l--) {
-            if(s.length()%l == 0) {
-                int times = s.length()/l;
-                StringBuilder sb = new StringBuilder();
-                while(times > 0) {
-                    sb.append(s.substring(0,l));
-                    times--;
-                }
-                if(s.equals(sb.toString()))    return true;
+
+        int n = s.length();
+        int[] z = getZarr(s);
+
+        for(int i = 1; i < n; i++) {
+
+            if(i + z[i] == n && n % i == 0) {
+                return true;
             }
         }
+
         return false;
+    }
+    /// //////////Helper////////////////////
+    private static int[] getZarr(String s) {
+        int z[] = new int[s.length()];
+        int l = 0, r = 0;
+
+        for(int i = 1; i < s.length(); i++) {
+            if(i <= r) {
+                z[i] = Math.min(r-i+1,z[i-l]);
+            }
+            while(i + z[i] <  s.length() && s.charAt(z[i]) == s.charAt(i+z[i])) {
+                z[i]++;
+            }
+            if(i + z[i] -1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
     }
 }

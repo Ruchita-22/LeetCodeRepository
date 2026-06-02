7e16573aa9ef95c1cea3cf4c830beb462458c17c
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-06-02 18:24:33 +0530

    Time: 9 ms (5.23%), Space: 43.4 MB (9.61%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int strStr(String  st, String pattern) {
+       String s = pattern+'$'+st;
+       int z[] = getZarr(s);
+        for(int i = 0; i < s.length(); i++) {
+            if(z[i] == pattern.length()) {
+                return i-pattern.length()-1;
+            }
+       }
+       return -1;
+    }
+    private static int[] getZarr(String s) {
+        int z[] = new int[s.length()];
+        int l = 0, r = 0;
+
+        for(int i = 1; i < s.length(); i++) {
+            if(i <= r) {
+                z[i] = Math.min(r-i+1,z[i-l]);
+            }
+            while(i + z[i] <  s.length() && s.charAt(z[i]) == s.charAt(i+z[i])) {
+                z[i]++;
+            }
+            if(i + z[i] -1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+}

c03191eb5a3579f8325c5546226064a5a0b85806
Ruchita-22 <ruchita.chandel22@gmail.com>
2026-06-07 02:57:53 +0530

    Time: 9 ms (36.72%), Space: 46.7 MB (56.94%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,33 +1,49 @@
 class Solution {
-    private static long add(long a, long b) {
-        long mod = 1000000007;
-        return ((a % mod) + (b % mod) + mod) % mod;
-    }
-    private static long sub(long a, long b) {
-        long mod = 1000000007;
-        return ((a % mod) - (b % mod) + mod) % mod;
-    }
-    private static long mul(long a, long b) {
-        long mod = 1000000007;
-        return ((a % mod) * (b % mod) + mod) % mod;
+ 
+    public String shortestPalindrome(String s) {
+
+        String rev = new StringBuilder(s).reverse().toString();
+        String temp = s + "$" + rev;
+        int[] z = zFunction(temp);
+
+        int longestPalPrefix = 0;
+
+        for (int i = 0; i < temp.length(); i++) {
+            if (i + z[i] == temp.length()) {
+                longestPalPrefix = Math.max(longestPalPrefix, z[i]);
+            }
+        }
+
+        String suffix = s.substring(longestPalPrefix);
+
+        return new StringBuilder(suffix).reverse().toString() + s;
     }
 
-    public String shortestPalindrome(String s) {
-        long FH = 0, BH = 0;
-        long p = 31;
-        long maxPower = 1;
-        int len = 0;
-        for(int i = 0; i < s.length(); i++) {
-            // FH = FH*p+c
-            FH = add(mul(FH , p) , s.charAt(i) - 'a'+1);
-            //BH = BH + c * p^max
-            BH = add(BH, mul(s.charAt(i) - 'a'+1, maxPower));
-            if(FH == BH) {
-                len = i+1;
+    private int[] zFunction(String s) {
+
+        int n = s.length();
+        int[] z = new int[n];
+
+        int l = 0;
+        int r = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= r) {
+                z[i] = Math.min(r - i + 1, z[i - l]);
             }
-            maxPower = mul(maxPower,p);
+
+            while (i + z[i] < n &&
+                   s.charAt(z[i]) == s.charAt(i + z[i])) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
         }
-        String st = s.substring(len);
-        return new StringBuilder(st).reverse().toString() + s;
+
+        return z;
     }
 }
